c07947b5fbce6840dd5594799a8b25699b68c66d
Tom Grubbe <tomg@tomgrubbe.com>
2025-05-22 19:49:32 -0700

    Expand bash template

--- templates/bash_init.sh
+++ templates/bash_init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-PROG=$(basename $0)
-CWD=$(cd $(dirname $0) && pwd)
+PROG=$(basename "$0")
+CWD=$(cd "$(dirname "$0")" && pwd)
+
+# Flags
+FILENAME="${CWD}/filename.txt"
 
 fail()
 {
@@ -11,9 +14,53 @@ fail()
   exit 1
 }
 
+usage()
+{
+cat <<EOF
+
+  USAGE: % ./${PROG} [options] 
+
+  OPTIONS:
+
+    -h                   - Usage
+    -f <filename>        - Some description
+    <add options here>
+
+  EXAMPLES:
+
+    % ./${PROG} <add flags here>
+
+EOF
+  exit 0
+}
+
+process_args()
+{
+  [ $# -lt  0 ] && usage
+
+  while getopts "hf:?" opt
+  do
+    case "$opt" in
+      h)
+        usage
+        ;;
+      f)
+        FILENAME=${OPTARG}
+        ;;
+      *)
+        echo "-- What? --"
+        usage
+        ;;
+    esac
+  done
+}
+
 Main()
 {
+  process_args "$@"
 
+  echo "$FILENAME"
 }
 
 Main "$@"
+
