030f42808274227cc78f0cdba335aac0d554e902
Tom Grubbe <tomg@tomgrubbe.com>
2025-05-26 23:11:39 -0700

    Use vim.keymap.set instead of vim.api.nvim_set_keymap

--- lua/tomg/core/keymaps.lua
+++ lua/tomg/core/keymaps.lua
@@ -3,7 +3,8 @@
 local opts = { noremap = true, silent = true }
 
 -- Convenient alias
-local keymap = vim.api.nvim_set_keymap
+--local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
 -- Splitter navigation
 keymap("n", "<C-Left>", "<C-w>h", opts)
